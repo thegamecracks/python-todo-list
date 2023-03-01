fba61cae2c9d16c2a824bb43052803a21bcde2cd
thegamecracks <61257169+thegamecracks@users.noreply.github.com>
2023-03-01 18:27:37 -0500

    Set up PostgreSQL database + .env config
    Instead of the initially planned TOML configuration, a regular
    .env file will be used. Each service is set to only have a subset
    of the evironment variables exposed to them.

--- README.md
+++ README.md
@@ -12,9 +12,7 @@ docker build --target tests .
 - [X] Set up Dockerfile / docker-compose.yml (test by printing "hello world!")
 - [X] Set up FastAPI app (test by accessing from Docker)
 - [X] Set up todo SQLAlchemy models
-- [ ] Add TOML configuration mechanism for specifying config and secrets
-  - Consider Pydantic models for de/serialization
-- [ ] Set up PostgreSQL database with docker (test by committing / fetching data)
+- [X] Set up PostgreSQL database with docker (test by committing / fetching data)
 - [ ] Create endpoints to interface with todo
 - [ ] Add website as front-end for API
 - [ ] Add user authentication via API tokens
--- src/todo_list/app.py
+++ src/todo_list/app.py
@@ -1,8 +1,37 @@
+import os
+
 from fastapi import FastAPI
+from sqlalchemy import text
+from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
+
+from .models import Base
 
 app = FastAPI()
+engine = create_async_engine(
+    "postgresql+asyncpg://{user}:{password}@{host}:{port}/{dbname}".format(
+        user=os.environ["POSTGRES_USER"],
+        password=os.environ["POSTGRES_PASSWORD"],
+        host="db",  # service name defined in docker-compose.yml
+        port=5432,
+        dbname=os.environ["POSTGRES_DB"],
+    )
+)
+sessionmaker = async_sessionmaker(engine, expire_on_commit=False)
+
+
+@app.on_event("startup")
+async def on_startup():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+
+
+@app.on_event("shutdown")
+async def on_shutdown():
+    await engine.dispose()
 
 
 @app.get("/")
 async def index():
-    return {"message": "Hello World"}
+    async with engine.begin() as conn:
+        result = await conn.execute(text("SELECT 'Hello world!'"))
+        return {"message": result.scalar_one()}
